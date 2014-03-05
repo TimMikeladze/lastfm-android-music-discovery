1b12108e4ff49f3a67ae6ec2b8aa645c237588c5
Tim Mikeladze <tim.mikeladze@gmail.com>
2014-03-04 17:23:50 -0800

    Added stubs for gridview

---
+++ src/com/mikeladze/musicdiscovery/view/gridview/ImageGridView.java
@@ -0,0 +1,22 @@
+package com.mikeladze.musicdiscovery.view.gridview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+
+public class ImageGridView extends GridView {
+
+    public ImageGridView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public ImageGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public ImageGridView(Context context) {
+        super(context);
+    }
+
+}
