c4390fc045b66e4a5936167be4186e08d50f90fd
TimMikeladze <tim.mikeladze@gmail.com>
2014-03-06 09:26:40 -0800

    Added style

--- gen/com/mikeladze/musicdiscovery/R.java
+++ gen/com/mikeladze/musicdiscovery/R.java
@@ -180,7 +180,39 @@ containing a value of this type.
         public static final int margin=0x7f050002;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ab_background_textured_style=0x7f020000;
+        public static final int ab_bottom_solid_style=0x7f020001;
+        public static final int ab_solid_style=0x7f020002;
+        public static final int ab_stacked_solid_style=0x7f020003;
+        public static final int ab_texture_tile_style=0x7f020004;
+        public static final int ab_transparent_style=0x7f020005;
+        public static final int btn_cab_done_default_style=0x7f020006;
+        public static final int btn_cab_done_focused_style=0x7f020007;
+        public static final int btn_cab_done_pressed_style=0x7f020008;
+        public static final int btn_cab_done_style=0x7f020009;
+        public static final int cab_background_bottom_style=0x7f02000a;
+        public static final int cab_background_top_style=0x7f02000b;
+        public static final int ic_launcher=0x7f02000c;
+        public static final int list_focused_style=0x7f02000d;
+        public static final int list_pressed_style=0x7f02000e;
+        public static final int menu_dropdown_panel_style=0x7f02000f;
+        public static final int progress_bg_style=0x7f020010;
+        public static final int progress_horizontal_style=0x7f020011;
+        public static final int progress_primary_style=0x7f020012;
+        public static final int progress_secondary_style=0x7f020013;
+        public static final int selectable_background_style=0x7f020014;
+        public static final int spinner_ab_default_style=0x7f020015;
+        public static final int spinner_ab_disabled_style=0x7f020016;
+        public static final int spinner_ab_focused_style=0x7f020017;
+        public static final int spinner_ab_pressed_style=0x7f020018;
+        public static final int spinner_background_ab_style=0x7f020019;
+        public static final int tab_indicator_ab_style=0x7f02001a;
+        public static final int tab_selected_focused_style=0x7f02001b;
+        public static final int tab_selected_pressed_style=0x7f02001c;
+        public static final int tab_selected_style=0x7f02001d;
+        public static final int tab_unselected_focused_style=0x7f02001e;
+        public static final int tab_unselected_pressed_style=0x7f02001f;
+        public static final int tab_unselected_style=0x7f020020;
     }
     public static final class id {
         public static final int action_settings=0x7f090004;
@@ -208,16 +240,11 @@ containing a value of this type.
         public static final int title_section3=0x7f060006;
     }
     public static final class style {
+        public static final int ActionBar_Solid_Style=0x7f070001;
+        public static final int ActionBar_Transparent_Style=0x7f070002;
+        public static final int ActionBarTabStyle_Style=0x7f070005;
+        public static final int ActionButton_CloseMode_Style=0x7f070008;
         /** 
-        Base application theme, dependent on API level. This theme is replaced
-        by AppBaseTheme from res/values-vXX/styles.xml on newer devices.
-    
-
-            Theme customizations available in newer API levels can go in
-            res/values-vXX/styles.xml, while customizations related to
-            backward-compatibility can go here.
-        
-
         Base application theme for API 11+. This theme completely replaces
         AppBaseTheme from res/values/styles.xml on API 11+ devices.
     
@@ -229,11 +256,15 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
-        /**  Application theme. 
- All customizations that are NOT specific to a particular API-level can go here. 
+        public static final int AppBaseTheme=0x7f07000a;
+        public static final int DropDownListView_Style=0x7f070004;
+        public static final int DropDownNav_Style=0x7f070006;
+        public static final int PopupMenu_Style=0x7f070003;
+        public static final int ProgressBar_Style=0x7f070007;
+        public static final int Theme_Style=0x7f070000;
+        /**  this style is only referenced in a Light.DarkActionBar based theme 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int Theme_Style_Widget=0x7f070009;
     }
     public static final class styleable {
         /** Attributes that can be used with a StaggeredGridView.
